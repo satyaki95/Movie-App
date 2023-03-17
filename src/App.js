f72a95d5f92c9ad4eac9f48bc0d4220a20228f39
satyaki95 <satyaki95@outlook.com>
2023-03-17 21:23:23 +0530

    call api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-
+import MovieList from './Components/MovieList';
 
 function App() {
+  const [movies, setMovies] = useState([ {
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+}
+]);
+
+const getMovieRequest = async () => {
+    const url = "http://www.omdbapi.com/?s=star wars&apikey=f2d5fe81";
+    const responce = await fetch(url);
+    const responceJson = await responce.json();
+    console.log(responceJson);
+};
+
+useEffect(()=>{
+    getMovieRequest();
+})
+
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className='container-fluid movie-app'>
+      <div className='row'>
+      <MovieList movies={movies} />
+      </div>
     </div>
   );
 }
