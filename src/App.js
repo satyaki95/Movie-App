b71c71dd17247de3dbe0eae729ad2b0c717975df
satyaki95 <satyaki95@outlook.com>
2023-03-18 14:40:48 +0530

    update cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './Component/Header/Header';
 import Home from './Pages/home/home';
+import MovieList from './Component/movieList/movieList';
 
 
 function App() {
@@ -12,8 +13,9 @@ function App() {
         <Routes>
           <Route index element={<Home />}></Route>
           <Route path="movie/:id" element={<h1>Movie Details</h1>}></Route>
-          <Route path="movies/:type" element={<h1>Movie List</h1>}></Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
           <Route path="movies/scarch" element={<h1>scarch</h1>}></Route>
+          <Route path="movies/favourite" element={<h1>Favourite</h1>}></Route>
           <Route path="/*" element={<h1>Error</h1>}></Route>
         </Routes>
       </Router>
