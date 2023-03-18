bec886e751d983ffdc5c549a7847bf283fd9bf36
satyaki95 <satyaki95@outlook.com>
2023-03-18 13:14:03 +0530

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './Component/Header/Header';
+import Home from './Pages/home/home';
+
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<h1>Movie Details</h1>}></Route>
+          <Route path="movies/:type" element={<h1>Movie List</h1>}></Route>
+          <Route path="movies/scarch" element={<h1>scarch</h1>}></Route>
+          <Route path="/*" element={<h1>Error</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
